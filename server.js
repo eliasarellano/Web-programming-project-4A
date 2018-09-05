2819a0623f34b00114484f75288855437533ba15
Glitch (gvc-web-programming) <none>
2018-09-05 18:58:19 +0000

    🏹🍈 Checkpoint ./server.js:1707659/2715

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var app = express();
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
+app.get('/login', function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
